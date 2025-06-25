e46693b4e23026b4e8ecd674e3858d500c5b6daf
oyone161214 <uuitiro@gmail.com>
2025-06-26 07:58:10 +0900

    app.pyコメント

--- app.py
+++ app.py
@@ -1,38 +1,60 @@
 from flask import Flask, render_template, url_for, request, redirect
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime, timezone
+from flask_sqlalchemy import SQLAlchemy  #データベース
+from datetime import datetime, timezone  #日付
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(app)
+#FlaskでWebアプリを始める
+app = Flask(__name__) 
+# app のDB保存先 test.db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'  
+#pythonでdbを使うためのライブラリを変数に入れる
+db = SQLAlchemy(app) 
 
+
+# DB用クラスの設定
+# db.Model = クラスをDBテーブルに対応させる
 class Todo(db.Model):
+    # Column(列名)の設定と制約
     id = db.Column(db.Integer, primary_key = True)
     content = db.Column(db.String(200), nullable = False)
     #completed = db.Column(db.Integer, default = 0)
+    ##データ型(DateTime)の制約、初期値は現在のUTC（世界標準時）
     date_created = db.Column(db.DateTime, default = datetime.now(timezone.utc))
 
+    #データの値を返す関数
     def __repr__(self):
         return '<Task %r>' % self.id
 
+#メインページが開かれたときに実行する関数index
+#GET  = データの取得（ページの表示）
+#POST = データの送信（新しいタスクの追加）
 @app.route('/', methods = ['POST', 'GET'])
 def index():
+    # index.html の form からタスク(content)が送られてきたとき
     if request.method == 'POST':
+        # content を取り出す
         task_content = request.form['content']
+        #Todoクラスの新しい content に task_content を追加する
         new_task = Todo(content = task_content)
 
-        try:
+        # try = エラーが出るかもしれない処理
+        #追加したTodoをDBに add⇒commit
+        try: 
             db.session.add(new_task)
             db.session.commit()
+            # メインページをリロード
             return redirect('/')
+        # error時
         except:
             return 'タスクの実行中に問題が発生しました'
 
+    # GET でページを表示をするだけのとき
     else:
+        # Todo クラスに対応する DB からタスクの作成日時順に、全部のデータを取り出す
         tasks = Todo.query.order_by(Todo.date_created).all()
+        # index.html に tasks のデータを組み込んで表示する
         return render_template('index.html', tasks = tasks)
     
-
+# DELETEボタンが押されたとき
 @app.route('/delete/<int:id>')
 def delete(id):
     task_to_delete = Todo.query.get_or_404(id)
@@ -44,8 +66,11 @@ def delete(id):
     except:
         return 'タスクの削除中に問題が発生しました'
     
+# UPDATEボタンが押されたとき
 @app.route('/update/<int:id>', methods = ['GET', 'POST'])
 def update(id):
+
+    #TodoからIDデータを取り出す。結果はGETか404エラー。
     task = Todo.query.get_or_404(id)
 
     if request.method == 'POST':
